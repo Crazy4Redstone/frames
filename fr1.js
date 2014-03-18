b0631ab212fa0c6cd4fa185ca9bd49157d818a47
Crazy4Redstone <mcpe1398@gmail.com>
2014-03-17 22:49:21 -0500

    Create fr1.js

---
+++ fr1.js
@@ -0,0 +1,10 @@
+// Item frames mod
+// Made by Crazy4Redstone
+
+// Items
+
+ModPE.setItem (389,'item_frame',0,'Item Frame')
+
+// Blocks
+
+Block.defineBlock (400,'Item Frame',['planks',0,'planks',0,)
